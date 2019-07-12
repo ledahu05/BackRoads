8668b2d04da2eacfda19b62df9a0189761486664
ledahu <christophe.seguinot@gmail.com>
2019-07-12 17:26:31 +0200

    Banner component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import SimleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
 export default () => (
     <Layout>
-        <SimleHero />
+        <SimleHero>
+            <Banner
+                title="continue exploring"
+                info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
+            >
+                <Link to="/tours" className="btn-white">
+                    explore tours
+        </Link>
+            </Banner>
+        </SimleHero>
     </Layout>
 )
