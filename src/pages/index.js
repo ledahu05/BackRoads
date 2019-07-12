90c59d8906ef2c1627f8253bf7e5533cf911e175
ledahu <christophe.seguinot@gmail.com>
2019-07-12 17:19:02 +0200

    SimpleHero Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import { FaBeer } from 'react-icons/fa';
-const index = () => {
-    return (
-        <Layout>
-            Index page <FaBeer />
-        </Layout>
-    )
-}
-export default index
+import SimleHero from "../components/SimpleHero"
+export default () => (
+    <Layout>
+        <SimleHero />
+    </Layout>
+)
