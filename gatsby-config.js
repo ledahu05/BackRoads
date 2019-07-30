dd5de1cc5f2e7ffcab15fc522f51f3a46425bf21
ledahu <christophe.seguinot@gmail.com>
2019-07-30 09:10:46 +0200

    add metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "BackRoads",
+    description:
+      "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
+    author: "@appliz",
+    data: ["item1", "item2"],
+  },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
 }
